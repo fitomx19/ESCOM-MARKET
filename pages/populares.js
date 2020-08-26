6594f0d455dedd5f17e21d5bc72b8b68d9ba188e
Adolfo <fitox.metallica@gmail.com>
2020-08-26 15:20:07 -0500

    Login y añadir productos completado en firebase

---
+++ pages/populares.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Layout from "../components/layout/Layout";
+
+const Populares = () => (
+  <div>
+    <Layout>
+      <h1>Populares</h1>
+    </Layout>
+  </div>
+);
+
+export default Populares;
